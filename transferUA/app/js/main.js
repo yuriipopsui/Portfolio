c775d8aad92447b6c3de1f04ce6c1db37b4f5d1c
Yurii Popsui <yp.onward@gmail.com>
2021-01-03 20:30:22 +0200

    Add TransferUa

---
+++ transferUA/app/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+ 
+var swiper = new Swiper ('.swiper-container', {
+
+  loop: true,
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }, 
+});
+
+});
